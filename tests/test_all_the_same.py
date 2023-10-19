5b63dfd3bacc107d1855087314bfe17d3a7fff9b
François Roussel <frouss01@centralelille.lan>
2023-10-19 15:22:26 +0200

    feat: improving code quality with the help of Pylint

--- tests/test_all_the_same.py
+++ tests/test_all_the_same.py
@@ -1,9 +1,14 @@
-from poetry_project.all_the_same import *
+"""Testing the all_the_same function"""
+
+from poetry_project.all_the_same import all_the_same_compute
 
 def test_all_the_same():
-    assert all_the_same_compute([1, 1, 1]) == True
-    assert all_the_same_compute([1, 2, 1]) == False
-    assert all_the_same_compute([1, "a", 1]) == False
-    assert all_the_same_compute([]) == True
-    assert all_the_same_compute([1]) == True
-    assert all_the_same_compute('not a list') == False
+    """Define the test function
+    Args: None
+    Returns: None"""
+    assert all_the_same_compute([1, 1, 1]) is True
+    assert all_the_same_compute([1, 2, 1]) is False
+    assert all_the_same_compute([1, "a", 1]) is False
+    assert all_the_same_compute([]) is True
+    assert all_the_same_compute([1]) is True
+    assert all_the_same_compute('not a list') is False
